8536ceab4ddc25d0fdf27ccaa06be16f48e57fbf
Josh Harbaugh <pixelhavenllc@gmail.com>
2025-02-19 10:38:44 -0500

    fix: resolve type error in DeleteInvoice button component.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -109,13 +109,8 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  try {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
-    return { message: 'Deleted Invoice.' };
-  } catch (error) {
-    return { message: 'Database Error: Failed to Delete Invoice.', error };
-  }
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
 
 export async function authenticate(
